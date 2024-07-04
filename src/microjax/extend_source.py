d70248b9804fdac126dd39035ab2891d51a6efd2
mshota <miyazaki@ir.isas.jaxa.jp>
2024-07-04 13:55:04 +0900

    temporal finish?

--- src/microjax/extend_source.py
+++ src/microjax/extend_source.py
@@ -12,9 +12,23 @@ def source_profile_limb1(dz, u1=0.0):
     mu = jnp.sqrt(1.0 - dz*dz)
     return 1 - u1 * (1.0 - mu)
 
-def image_area_binary(w_center, z_inits, q, s, rho, NBIN=20, max_iter=100000):
+def image_area_binary(w_center, z_inits, q, s, rho, NBIN=10, max_iter=100000):
+    """ 
+    Compute the area of the image formed by a binary lens system.
 
-    # used arrays
+    Args:
+        w_center (complex): Position of the source in the complex plane.
+        z_inits (array_like): Initial guesses for the positions of the images in the complex plane.
+        q (float): Mass ratio of the binary lens components.
+        s (float): Separation between the two lenses.
+        rho (float): Radius of the source.
+        NBIN (int, optional): Number of bins used for discretization in y-direction. Defaults to 20.
+        max_iter (int, optional): Maximum number of iterations for the algorithm. Defaults to 100000.
+
+    Returns:
+        float: Total area of all images produced by the lens system.
+    """
+    # Initialize arrays to store intermediate results and indices
     indx   = jnp.zeros((max_iter * 2, 4), dtype=int)
     Nindx  = jnp.zeros((max_iter * 2,),   dtype=int)
     xmax   = jnp.zeros((max_iter * 4,))
@@ -22,20 +36,23 @@ def image_area_binary(w_center, z_inits, q, s, rho, NBIN=20, max_iter=100000):
     area_x = jnp.zeros((max_iter * 4,))
     y      = jnp.zeros((max_iter * 4,))
     dys    = jnp.zeros((max_iter * 4,))
+    area   = 0.0
 
     nimage  = len(z_inits)
     overlap = jnp.zeros(6) # binary-lens
     incr    = rho / NBIN  
     incr2   = 0.5 * incr 
     incr2margin = incr2 * 1.01  
-    area_i  = jnp.zeros(6)
     
     if rho <= 0:
         return 0.0
     
-    for i in range(1, nimage+1):
+    area_image = jnp.zeros(nimage)
+    for i in range(nimage):
+    #for i in range(1, nimage+1):
         if overlap[i] == 1:
             continue
+        area_i = 0.0
         # search image toward +y
         yi = 0
         dy = incr
@@ -44,7 +61,8 @@ def image_area_binary(w_center, z_inits, q, s, rho, NBIN=20, max_iter=100000):
         xmax = xmax.at[0].set(z_init[i].real)
         carry = (yi, indx, Nindx, xmax, xmin, area_x, y, dys) 
         area_y_plus, carry = image_area0_binary(w_center, z_init, q, s, dy, carry)
-        
+        area_i += area_y_plus
+
         # search image toward -y
         (yi, indx, Nindx, xmax, xmin, area_x, y, dys) = carry  
         dy       = -incr
@@ -56,6 +74,7 @@ def image_area_binary(w_center, z_inits, q, s, rho, NBIN=20, max_iter=100000):
         yi  += 1
         carry = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
         area_y_minus, carry = image_area0_binary(w_center, z_init, q, s, dy, carry)
+        area_i += area_y_minus
 
         # search extra image beyond the boundary
         (yi, indx, Nindx, xmax, xmin, area_x, y, dys) = carry  
@@ -64,41 +83,94 @@ def image_area_binary(w_center, z_inits, q, s, rho, NBIN=20, max_iter=100000):
         for j in range(nyi):
             dxmax = xmax[j + 1] - xmax[j]
             dxmin = xmin[j + 1] - xmin[j]
-            if dxmax > 1.1 * incr:
-                z = jnp.complex128(xmax[j + 1] + 1j *  y[j])
-                xmin = xmin.set[yi].set(xmax[j])
-                xmax = xmax.set[yi].set(xmax[j + 1])
-                dy   = -dy
-                yi  += 1
+            if area_x[j+1] > 0.0:
+                if dxmax > 1.1 * incr:
+                    z_init = jnp.complex128(xmax[j + 1] + 1j *  y[j])
+                    xmin   = xmin.set[yi].set(xmax[j])
+                    xmax   = xmax.set[yi].set(xmax[j + 1])
+                    dy     = -dys[j]
+                    dys    = dys.at[yi].set(dy)
+                    yi    += 1
+                    
+                    carry        = (yi, indx, Nindx, xmax, xmin, area_x, y, dys) 
+                    area0, carry = image_area0_binary(w_center, z_init, q, s, dy, carry)
+                    area_i      += area0
+                    area_bound  += area0
+                    if area0 <= 0:
+                        yi -= -1
+                if dxmin > 1.1 * incr:
+                    z_init = jnp.complex128(xmax[j + 1] - incr + 1j *  y[j])
+                    xmin   = xmin.set[yi].set(xmin[j])
+                    xmax   = xmax.set[yi].set(xmin[j + 1])
+                    dy     = dys[j]
+                    dys    = dys.at[yi].set(dy)
+                    yi    += 1
 
+                    carry        = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
+                    area0, carry = image_area0_binary(w_center, z_init, q, s, dy, carry)
+                    area_i      += area0
+                    area_bound  += area0
+                    if area0 <= 0:
+                        yi -= -1
+                if dxmin < -1.1 * incr:
+                    z_init = jnp.complex128(xmin[j] - incr + 1j *  y[j])
+                    xmin   = xmin.set[yi].set(xmin[j + 1])
+                    xmax   = xmax.set[yi].set(xmin[j])
+                    dy     = -dys[j]
+                    dys    = dys.at[yi].set(dy)
+                    yi    += 1
 
+                    carry        = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
+                    area0, carry = image_area0_binary(w_center, z_init, q, s, dy, carry)
+                    area_i      += area0
+                    area_bound  += area0
+                    if area0 <= 0:
+                        yi -= -1
+            # search boundary again if new image area was found
+            if j == nyi -1 and area_bound > 0.0 and yi > nyi:
+                nyi = yi
+        
+        area += area_i # total area of all images
+        area_image = area_image.at[i].set(area_i) # area of image i
 
-
-
+        # search overlapping images
+        for ii in range(nimage): #  roop for images
+        #for ii in range(1, nimage + 1): #  roop for images
+            if ii == i:
+                continue
+            for j in range(nyi): # roop for boundaries
+                if area_x[j] <= 0.0:
+                    continue
+                if z_inits[ii].imag >= y[j] - incr2margin and z_inits[ii].imag <= y[j] + incr2margin:
+                    if z_inits[ii].real >= xmin[j] - incr2margin and z_inits[ii].real <= xmax[j] + incr2margin:
+                        if ii < i: # already searched image
+                            area -= area_image[ii]
+                        else:
+                            overlap = overlap.at[ii].set(1.0)
+                        break
+        # initialize index for checking roop image
+        for j in range(nyi):
+            index = int(y[j] / incr + max_iter)
+            Nindx = Nindx.at(index).set(0)
+    
+    return area
 
 def image_area0_binary(w_center, z_init, q, s, rho, dy, carry):
-    """
-    calculate an image area with binary-lens by inverse-ray shooting.
+    """ 
+    Auxiliary function to calculate area of an image for binary lens system by inverse-ray shooting.
 
     Args:
-        w_center (complex)  : The complex position of the source.
-        z_init (complex)    : Initial position of the image point.
-        q (float)           : Mass ratio of the binary lens.
-        s (float)           : Separation between the two lenses.
-        rho (float)         : Radius of the source.
-        dy (float,opt)      : Step size in the y-direction, default is 1e-4.
-        carry (taple)       :
-            indx (array_like)   : index for x
-            Nindx (array_like)  : 
-            xmin (array_like)   :
-            xmax (array_like)   :
-            area_x (array_like) :
-            y (array_like)      :
-            dys (array_like)    :
+        w_center (complex): Position of the source in the complex plane.
+        z_init (complex): Initial position of the image point.
+        q (float): Mass ratio of the binary lens.
+        s (float): Separation between the two lenses.
+        rho (float): Radius of the source.
+        dy (float): Step size in the y-direction.
+        carry (tuple): Tuple containing arrays and indices for ongoing calculations.
+
     Returns:
-        float: Total brightness of the image area, 
-                proportional to its physical area and adjusted for limb darkening.
-        taple: Arrays for calculating the overlaping or etc...
+        float: Total brightness of the image area, adjusted for limb darkening.
+        tuple: Updated carry containing intermediate results for continued calculations.
     """
     indx, Nindx, xmax, xmin, area_x, y, dys = carry 
     max_iter = int(len(dys) / 4.0)
@@ -157,7 +229,7 @@ def image_area0_binary(w_center, z_init, q, s, rho, dy, carry):
                     if xmin[yi] + incr < xmax[ind] and xmax[yi] - incr > xmin[ind]: # already counted.
                         return count_all - count_x 
                 # save index yi if counted
-                indx = indx.at[[y_index][Nindx[y_index]]].set(yi)
+                indx = indx.at[y_index, Nindx[y_index]].set(yi)
                 Nindx = Nindx.at[y_index].add(1)
                 # move next y-row 
                 yi += 1
