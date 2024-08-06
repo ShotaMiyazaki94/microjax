64f016d1505318c36319c87434066b9f677158ed
mshota <miyazaki@ir.isas.jaxa.jp>
2024-08-06 15:02:44 +0900

    move but not working...

--- tests/integrate/inverse_ray/test_image_area0.py
+++ tests/integrate/inverse_ray/test_image_area0.py
@@ -4,18 +4,18 @@ jax.config.update("jax_enable_x64", True)
 import matplotlib.pyplot as plt
 
 from microjax.point_source import lens_eq, _images_point_source, critical_and_caustic_curves
-from microjax.inverse_ray import image_area0, image_area_all, CarryData
+from microjax.image_area0 import image_area0
 
 NBIN=100
 nlenses=2
 
-w_center = jnp.complex128(-0.05 - 0.1j)
-q  = 0.5
+w_center = jnp.complex128(0.3 - 0.1j)
+q  = 0.1
 s  = 1.0
 a  = 0.5 * s
 e1 = q / (1.0 + q) 
 _params = {"q": q, "s": s, "a": a, "e1": e1}
-rho = 0.13
+rho = 0.18
 
 incr  = jnp.abs(rho / NBIN)
 incr2 = incr * 0.5
@@ -44,26 +44,21 @@ count_all = 0.0
 rho2 = rho * rho
 finish = jnp.bool_(False)
 
-z_init = z_inits[z_mask][1]
-dy     = incr
-carry  = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
-area, carry = image_area0(w_center, rho, z_init, dy, carry, **_params)
-(yi, indx, Nindx, xmax, xmin, area_x, y, dys) = carry
-print(area, yi)
-print(y)
-print(dy)
-print(area_x)
 
-dy     = -incr
-carry  = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
-area, carry = image_area0(w_center, rho, z_init, dy, carry, **_params)
-(yi, indx, Nindx, xmax, xmin, area_x, y, dys) = carry
-print(area, yi)
-print(y)
-print(dy)
-print(area_x)
+for i in range(len(z_inits[z_mask])):
+    z_init = z_inits[z_mask][i]
+    dy     = incr
+    carry  = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
+    area, carry = image_area0(w_center, rho, z_init, dy, carry, **_params)
+    (yi, indx, Nindx, xmax, xmin, area_x, y, dys) = carry
+    print(area)
+    dy     = -incr
+    carry  = (yi, indx, Nindx, xmax, xmin, area_x, y, dys)
+    area, carry = image_area0(w_center, rho, z_init, dy, carry, **_params)
+    (yi, indx, Nindx, xmax, xmin, area_x, y, dys) = carry
+    print(area)
 
-N_limb = 5000
+N_limb = 1000
 w_limb = w_center + jnp.array(rho * jnp.exp(1.0j * jnp.pi * jnp.linspace(0.0, 2*jnp.pi, N_limb)), dtype=complex)
 w_limb_shift = w_limb - 0.5*s*(1 - q)/(1 + q) # half-axis coordinate
 image, mask = _images_point_source(w_limb_shift, a=a, e1=e1) # half-axis coordinate
@@ -82,6 +77,11 @@ for i in range(len(xmin[mask_x])):
 
 source = plt.Circle((w_center.real, w_center.imag), rho, color='b', fill=False)
 ax.add_patch(source)
+plt.plot(w_center.real, w_center.imag, "*", color="k")
+
+plt.plot(-q * s, 0 , ".",c="k")
+plt.plot((1.0 - q) * s, 0 ,".",c="k")
+
 plt.scatter(image_limb[mask].ravel().real, image_limb[mask].ravel().imag, s=1,color="purple")
 plt.scatter(cau_tri.ravel().real, cau_tri.ravel().imag,   marker=".", color="red", s=1)
 plt.scatter(crit_tri.ravel().real, crit_tri.ravel().imag, marker=".", color="green", s=1)
