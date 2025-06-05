6b398365315488483552c973d2cb0491e3a9699a
ShotaMiyazaki94 <miyazaki@ir.isas.jaxa.jp>
2025-06-05 17:32:01 +0900

    try hmc with rho

--- example/OGLE-2014-BLG-1722/3x_plot.py
+++ example/OGLE-2014-BLG-1722/3x_plot.py
@@ -6,7 +6,8 @@ import numpy as np
 # idata 読み込み
 idata = az.from_netcdf("example/OGLE-2014-BLG-1722/mcmc_full.nc")
 
-param_names = ['alpha', 'log_q', 'log_q3', 'log_s', 'log_s2', 'log_tE', 'piEE', 'piEN', 'psi', 't0_diff', 'u0']
+param_names = ['t0_diff', 'u0', 'log_tE', 'log_q', 'log_s', 'alpha',
+                'piEN', 'piEE', 'log_q3', 'log_s2', 'psi']
 
 posterior_samples = idata.posterior
 samples = np.concatenate(
@@ -14,7 +15,19 @@ samples = np.concatenate(
     axis=1
 )
 
-labels = [f"${name}$" for name in param_names]
+labels = [
+    r"$t_0^\prime$",        # プライム記号は右上に
+    r"$u_0$",               # サブスクリプト付き
+    r"$\log t_E$",          # 対数は roman に
+    r"$\log q$",            
+    r"$\log s$",            
+    r"$\alpha$",            # ギリシャ文字
+    r"$\pi_{E,N}$",         # 下付き添字にカンマ（E方向）
+    r"$\pi_{E,E}$",         # N方向
+    r"$\log q_3$",          
+    r"$\log s_2$",          
+    r"$\psi$",              
+]
 figure = corner.corner(samples, labels=labels, show_titles=True,
                        title_kwargs={"fontsize": 12}, label_kwargs={"fontsize": 14})
 
